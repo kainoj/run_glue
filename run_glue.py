9a9b20f08d8cdc9947095173df8cab3c9f88b977
Przemek <kainoj@users.noreply.github.com>
2022-03-10 18:39:59 +0900

    src/{run_glue -> runner}.py

--- run_glue.py
+++ run_glue.py
@@ -5,7 +5,7 @@ from omegaconf import DictConfig
 @hydra.main(config_path="configs/", config_name="defaults.yaml")
 def main(config: DictConfig):
 
-    from src.run_glue import run_glue
+    from src.runner import run_glue
     from src.utils import print_config
 
     print_config(config, resolve=True)
